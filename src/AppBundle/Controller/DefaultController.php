25dd17bbaa69eeba3e9b772ee57905f73543dca7
josevavia <josevavia@josevavia.es>
2014-12-26 19:46:36 +0100

    Initial structure for a synfony project

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+
+    public function ocrAction()
+    {
+        return $this->render('default/ocr.html.twig');
+    }
+}
